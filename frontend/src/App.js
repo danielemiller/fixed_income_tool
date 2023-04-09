392dadb952571800f636f8950d5a39becdc9e7ec
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2023-04-09 15:09:20 -0400

    Render all components within main app.js component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputForm from './components/InputForm';
 import MetricsSelection from './components/MetricsSelection';
 import DataDisplay from './components/DataDisplay';
 import ErrorHandling from './components/ErrorHandling';
 
-function App() {
+const App = () => {
+  const [bondData, setBondData] = useState({});
+  const [selectedMetrics, setSelectedMetrics] = useState({});
+  const [calculatedData, setCalculatedData] = useState({});
+  const [error, setError] = useState(null);
+
+  const handleFormSubmit = (data) => {
+    setBondData(data);
+    // Here you can call your API to process the bond data and calculate the metrics
+    // Update the calculatedData state with the response from the API
+  };
+
+  const handleMetricsChange = (metricId, isSelected) => {
+    setSelectedMetrics({ ...selectedMetrics, [metricId]: isSelected });
+  };
+
+  const fetchDataAndCalculateMetrics = () => {
+    // Fetch data from the API and calculate the metrics based on bondData and selectedMetrics
+    // Update the calculatedData state with the calculated metrics
+    // If an error occurs, update the error state
+  };
+
   return (
-    <div>
-      <InputForm />
-      <MetricsSelection />
-      <DataDisplay />
-      <ErrorHandling />
+    <div className="App">
+      <h1>Fixed Income Tool</h1>
+      <InputForm onSubmit={handleFormSubmit} />
+      <MetricsSelection onChange={handleMetricsChange} />
+      <button onClick={fetchDataAndCalculateMetrics}>Calculate Metrics</button>
+      <DataDisplay data={calculatedData} />
+      <ErrorHandling error={error} />
     </div>
   );
-}
+};
 
 export default App;
