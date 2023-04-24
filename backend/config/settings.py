102ab89a6bc47d5f3568ff34b928de8ad498ae02
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2023-04-24 00:20:56 -0400

    Add CORS middleware to settings.py to allow communication between frontend app and backend app.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'api',
     'data_storage',
     'dbbackup',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +62,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://fixed-income-tool-frontend.herokuapp.com",
 ]
 
 ROOT_URLCONF = 'config.urls'
