8f31da8b68f5b5b3254054216fae8f64b384436d
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2023-04-24 21:31:53 -0400

    Add Header component to frontend react application and adjust spacing between input form fields in the same grid row.

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './styles/Header.css';
+import logo from './images/logo.png';
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <img src={logo} alt="Logo" />
+        <h1 className="header-title">Fixed Income App</h1>
+      </div>
+      <nav className={`header-nav ${isMenuOpen ? 'header-nav-open' : ''}`}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/documentation">Documentation</a>
+          </li>
+          <li>
+            <a href="/blog">Fixed Income Blog</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="hamburger-menu" onClick={handleMenuClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
